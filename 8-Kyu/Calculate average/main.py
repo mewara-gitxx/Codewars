8934d19fe3d5a7113804617a5c5b4ecb72299a08
Aditya Mewara <mewaraaditya175@gmail.com>
2024-03-27 13:09:15 +0530

    Added few more questions

---
+++ 8-Kyu/Calculate average/main.py
@@ -0,0 +1,16 @@
+"""
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+"""
+
+
+
+
+def find_average(numbers):
+    # your code here
+    if len(numbers) <= 0:
+        return 0
+    else:
+        return sum(numbers) / len(numbers)
---
+++ 8-Kyu/Convert a Number to a String!/main.py
@@ -0,0 +1,9 @@
+"""
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+"""
+
+def number_to_string(num):
+    # Return a string of the number here!
+    return str(num)
